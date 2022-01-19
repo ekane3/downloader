29e821037c9abed441d354e2a1928e60ce23f5a2
EKANE <ekaneekane3@gmail.com>
2022-01-19 11:26:55 +0100

    [ADD] streamlit_player

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 import streamlit as st
+from streamlit_player import st_player
 
 
 # Description function
@@ -9,6 +10,22 @@ def description_app():
     st.markdown("""
             - Entrer juste le lien de la vidéo et cliquer sur le bouton 🔍 pour commencer le téléchargement
                 """)
+
+# Download function
+def download_video():
+    video = " "
+    st.markdown(' ')
+    st.markdown("### Download video 📥")
+    url = st.text_input("Enter the url of the video")
+    st_player("https://youtu.be/CmSKVW1v0xM")
+    if url:
+        try:
+            yt = YouTube(url)
+            video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+            st.success("Video downloaded")
+        except:
+            st.error("Error")
+    st.download_button(label="Download ",data=video)
 # Title bar function
 def title_bar():
     project_title = st.sidebar.text_input("You can change the title of this project by input")
@@ -32,10 +49,12 @@ def main():
         }
     )
     st.sidebar.title("Currently streaming with ❤")
-    genre = st.sidebar.radio("👇",('App description', 'Download Now'))
     st.sidebar.write(" ")
     st.sidebar.write(" ")
     st.sidebar.markdown("Made by [Emile.E](https://github.com/ekane3)")
+
+    description_app()
+    download_video()
 
 if __name__ == "__main__":
     main()
