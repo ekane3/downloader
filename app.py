d36630195f136e555c9b097c4994648dc230a14a
EKANE <ekaneekane3@gmail.com>
2022-01-19 15:15:01 +0100

    [UPDATE] readme file name changed

--- app.py
+++ app.py
@@ -7,26 +7,53 @@ from streamlit_player import st_player
 # Date and Time of the day function 
 def get_date_time():
     today = datetime.now()
-    st.markdown("### Date and Time 📆")
-    st.markdown(today.strftime("%A, %d %B %Y, %H:%M:%S"))
+    # create 3 columns
+    col1,col2,col3 = st.columns(3)
+    with col2:
+        st.markdown(' ')
+        st.markdown(' ')
+        st.markdown(' ')
+        st.markdown(' ')
+        #st.markdown("### Date and Time 📆")
+        st.markdown("##### 📆" +" "+ today.strftime("%d %B %Y "))
+        st.markdown("# "+today.strftime(" %H:%M:%S"))
     
 # Description function
 def description_app():
     st.title("Stream Media downer 📈")
-    st.markdown("Telecharger des vidéos sur YouTube")
-    st.markdown("""
-            - Entrer juste le lien de la vidéo et cliquer sur le bouton 🔍 pour commencer le téléchargement
-                """)
+    st.markdown("Télécharger des vidéos sur YouTube")
+    st.code("Entrer juste le lien de la vidéo et cliquer sur le bouton 🔍 pour commencer le téléchargement")
 
 # Download function
 def download_video():
     video = " "
+    DOWNLOAD_FOLDER = "./downloads"
     st.markdown(' ')
     st.markdown("### Download video 📥")
-    url = st.text_input("Enter the url of the video","https://youtu.be/CmSKVW1v0xM")
-    st_player(url)
+    st.markdown(' ')
+    st.markdown(' ')
+    col1,col2 = st.columns(2)
     
-    st.download_button(label="Download ",data=video)
+    with col1:
+        url = st.text_input("Enter the url of the video","https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        st_player(url) 
+        
+        #st.download_button(label="Download ",data=video)
+        if st.button("Download video"):
+            try:
+                video_obj = YouTube(url)
+                video_stream = video_obj.streams.get_highest_resolution()
+                video_stream.download(DOWNLOAD_FOLDER)
+            except:
+                st.error("Error")
+
+    with col2:
+        url2 = st.text_input("Enter the url of the video ","https://youtu.be/CmSKVW1v0xM")
+        st_player(url2) 
+        st.download_button(label="Download2 ",data=video)
+        if st.button('Say hello'):
+            st.write('Why hello there')
+
 # Title bar function
 def title_bar():
     project_title = st.sidebar.text_input("You can change the title of this project by input")
--- trash.py
+++ trash.py
@@ -5,3 +5,18 @@ if url:
             st.success("Video downloaded")
         except:
             st.error("Error")
+
+
+DOWNLOAD_FOLDER = "./downloads"
+    video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    video_obj = YouTube(video_url)
+    video_stream = video_obj.streams.get_highest_resolution()
+    video_stream.download(DOWNLOAD_FOLDER)
+
+    if st.button("Download video"):
+        try:
+            yt = YouTube(video_url)
+            video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(DOWNLOAD_FOLDER)
+            st.success("Video downloaded")
+        except:
+            st.error("Error")
